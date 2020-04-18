f62d11274376605f0dba724cb8bb4dc2f117568c
Nick Kaczmarek <nick.kaczmarek@hyland.com>
2020-04-18 13:56:18 -0400

    search for gif by title

--- src/components/saved.js
+++ src/components/saved.js
@@ -12,6 +12,9 @@ function Saved() {
   const { isAuthenticated, user: auth0User } = useAuth0();
   const [images, setImages] = useState();
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredImages, setFilteredImages] = useState([]);
+
   useEffect(() => {
     function localStorageGifMerge() {
       //if there are gifs to merge with
@@ -23,10 +26,7 @@ function Saved() {
         //save to firebase
         console.log("copying and deleting");
         const gifs = [...images, ...localStorageGifs];
-        const gif_Ids = [
-          ...images.map((g) => g.id),
-          ...localStorageGifs.map((g) => g.id),
-        ];
+        const gif_Ids = [...images.map((g) => g.id), ...localStorageGifs.map((g) => g.id)];
         updateGifsForUser({
           userEmail: auth0User.email,
           gifs: distinct(gifs),
@@ -53,6 +53,7 @@ function Saved() {
             };
           });
           setImages(gifs);
+          setFilteredImages(gifs);
         } catch (error) {
           console.error(error);
         }
@@ -61,39 +62,55 @@ function Saved() {
     getUserFromFirebase();
   });
 
+  useEffect(() => {
+    if (searchTerm) {
+      const results = images.filter((x) => x.title.toLowerCase().includes(searchTerm));
+
+      setFilteredImages(results);
+    } else {
+      setFilteredImages(images);
+    }
+  }, [searchTerm]);
+
   return (
-    <React.Fragment>
-      {images && images.length ? (
-        <section className="gif-container">
+    <>
+      <input
+        placeholder="Search your saved gifs"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      {filteredImages && filteredImages.length ? (
+        <>
           {" "}
-          {images.map((img) => (
-            <figure key={img.id}>
-              <img
-                className={"image"}
-                src={isSafari() ? img.safari : img.webp}
-                alt={img.title}
-                id={img.id}
-                onClick={() => copyImageUrlToClipboard(img)}
-              />
-              <button
-                className="remove-image"
-                onClick={async () => {
-                  let fbUser = await getGifsForUser(auth0User.email);
-                  setImages(fbUser.gifs.filter((x) => x.id !== img.id));
-                  await deleteGif(fbUser, img);
-                }}
-              >
-                X
-              </button>
-            </figure>
-          ))}
-        </section>
+          <section id="gif-container">
+            {filteredImages.map((img) => (
+              <figure key={img.id}>
+                <span>{img.title}</span>
+                <img
+                  className={"image"}
+                  src={isSafari() ? img.safari : img.webp}
+                  alt={img.title}
+                  id={img.id}
+                  onClick={() => copyImageUrlToClipboard(img)}
+                />
+                <button
+                  className="remove-image"
+                  onClick={async () => {
+                    let fbUser = await getGifsForUser(auth0User.email);
+                    setImages(fbUser.gifs.filter((x) => x.id !== img.id));
+                    await deleteGif(fbUser, img);
+                  }}
+                >
+                  X
+                </button>
+              </figure>
+            ))}
+          </section>
+        </>
       ) : (
-        <h1>
-          In order to have saved gifs you need to Sign in => Search => Click Gif
-        </h1>
+        <h1>In order to have saved gifs you need to Sign in => Search => Click Gif</h1>
       )}
-    </React.Fragment>
+    </>
   );
 }
 export { Saved };
